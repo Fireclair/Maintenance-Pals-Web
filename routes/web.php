1f93129d5d20d6e03703964ff5afba09a1a19591
Den\Dennn 😄 <danialaqil222@gmail.com>
2023-06-19 00:52:57 +0800

    Login

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('Mainpage');
     })->name('dashboard');
 });
 
